8cb76670063c200d7bbd4a81adfafdc1e26047f3
tvThejohasshen velusamy <tvthejohasshen@gmail.com>
2024-03-08 06:25:13 +0000

    Add Mongo1

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const cors = require ('cors');
 const mongodb = require ('mongodb');
+require('dotenv').config();
+
+// A Mongo client allows Express (or any
+// NodeJS application) to send request
+// to a Mongo database
 
 const MongoClient =mongodb.MongoClient;
+
+// create the express application
 const app = express();
 
+// enable cors
 app.use(cors());
+
+// set JSON as the means of
+// receiving requests and sending responses
 app.use(express.json());
 
+// function to connect to the database
 async function connect(uri , dbname) {
+
+     // `connect` allows us to connect to the mongodb
+    // useUnifiedTopology means we want use the latest
+    // structure for Mongo
     const client = await MongoClient.connect(uri,{
         useUnifiedTopology: true
     });
@@ -17,19 +33,63 @@ async function connect(uri , dbname) {
 }
 
 async function main() {
-    const uri = "mongodb+srv://root:current5@cluster0.blqnvaq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-    const db = await connect (uri,"sample_mflix");
-    app.get("/", async function(req,res){
-        const results = await db.collection("movies").find({}).limit(10).toArray();
+    // connection string is now from the .env file
+    // get the database using the `connect` function
+    const db = await connect(uri, "sctp02_food_sightings");
+    // create the routes after connecting to the database
+    app.get("/food-sightings", async function (req, res) {
+        try {
+            // get all the sightings
+            const results = await db.collection("sightings").find({}).toArray();
 
-        res.json({
-            'movies': results
-        })
+            res.json({
+                'sightings': results
+            })
+        } catch (e) {
+            res.status(500);
+            res.json({
+                'error': e
+            })
+        }
+
+    });
+
+    // Sample Food Sighting document"
+    // {
+    //   _id: ObjectId(),
+    //  description:"Chinese Buffet at LT2",
+    //  food:["fried rice", "chicken wings"],
+    //  datetime:2024-08-03
+    // }
+    app.post("/food-sighting", async function (req, res) {
+        // try...catch is for exception handling
+        // an execption is an unexpected error usually from a third party
+        // (in this case, the third party is Mongo Atlas)
+        try {
+            const description = req.body.description;
+            const food = req.body.food;
+            const datetime = new Date(req.body.datetime) || new Date();
+            const result = await db.collection("sightings").insertOne({
+                'description': description,
+                'food': food,
+                'datetime': datetime
+            });
+            res.json({
+                'result': result
+            })
+        } catch (e) {
+            // e will contain the error message
+            res.status(500); // internal server error
+            res.json({
+                'error': e
+            })
+        }
+
     })
 }
 
 main();
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server has started");
 });
