3f5364f09ebc36862abb9db4e8cd38c1fff7669d
tvThejohasshen velusamy <tvthejohasshen@gmail.com>
2024-03-08 06:08:15 +0000

    Added Mongo1

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ app.use(cors());
 app.use(express.json());
 
 async function connect(uri , dbname) {
-    const client = awaitMongoClient.connect(uri,{
+    const client = await MongoClient.connect(uri,{
         useUnifiedTopology: true
     });
     let db = client.db(dbname);
     return db;
 }
+
 async function main() {
-    const uri = mongodb+srv://root:current5@cluster0.blqnvaq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0;
+    const uri = "mongodb+srv://root:current5@cluster0.blqnvaq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
     const db = await connect (uri,"sample_mflix");
     app.get("/", async function(req,res){
         const results = await db.collection("movies").find({}).limit(10).toArray();
